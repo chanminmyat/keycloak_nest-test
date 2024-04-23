2e9f109ffcd8f32ebf343e736c1817c04cac6e5d
min <Chan.m@rznet.com>
2024-04-23 16:00:34 +0700

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get } from '@nestjs/common';
+import { AuthenticatedUser, Public, Roles, RoleMatchingMode, Unprotected } from 'nest-keycloak-connect';
+
+@Controller()
+export class AppController {
+  @Get()
+  @Public(false)
+  getHello(
+    @AuthenticatedUser()
+    user: any,
+  ): string {
+    if (user) {
+      return `Hello ${user.preferred_username}`;
+    } else {
+      return 'Hello world!';
+    }
+  }
+
+  @Get('private')
+  getPrivate() {
+    return 'Authenticated only!';
+  }
+
+  @Get('admin')
+  @Roles({ roles: ['admin'], mode: RoleMatchingMode.ALL })
+  adminRole() {
+    return 'Admin only!'
+  }
+}
